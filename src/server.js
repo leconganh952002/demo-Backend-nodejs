8d66a8459a790217e0c48b58f97c4bdb8daaf3a7
Le Cong Anh <huuanh952002@gmail.com>
2024-04-30 01:39:00 +0700

    video 24

--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 const express = require('express');
-const path = require('path');
 require('dotenv').config()
-
-
-
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+configViewEngine(app);
 
-
-app.get('/', (req, res) => {
-    res.send('Hello Cong Anh !')
-})
-
-app.get('/abc', (req, res) => {
-    res.send('Check ABC')
-})
-
-app.get('/hello', (req, res) => {
-    res.render('sample.ejs')
-})
+app.use('/', webRoutes);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
